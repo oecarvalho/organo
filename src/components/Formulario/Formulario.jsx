192c85fe60190f8b7b0c745bfe0b963830c5410f
Carvalho <felipe.carvalho.dm@gmail.com>
2024-01-02 10:50:23 -0300

    adicionando select

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,7 +1,19 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa/ListaSuspensa";
 
 
 const Formulario = () =>{
+
+    const times = [
+        'Programação',
+        'UX e Design',
+        'Front-End',
+        'Data Science',
+        'DevOps',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return(
         <section className="formulario">
             <form>
@@ -9,6 +21,7 @@ const Formulario = () =>{
                 <CampoTexto label='Nome' placeholder='Digite o seu nome'/>
                 <CampoTexto label='Cargo' placeholder='Digite o seu cargo'/>
                 <CampoTexto label='Imagem' placeholder='Informe o endereço da Imagem'/>
+                <ListaSuspensa label='Time' itens = {times}/>
             </form>
         </section>
     )
