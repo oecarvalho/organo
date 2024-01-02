b8981b938dcd452c523ea30824bb5ed74f810c70
Carvalho <felipe.carvalho.dm@gmail.com>
2024-01-02 16:18:55 -0300

    adicionando sessão times

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Banner from "./components/Banner/Banner"
 import Formulario from "./components/Formulario/Formulario"
+import Time from "./components/Time/Time"
 
 function App() {
 
@@ -15,6 +16,9 @@ function App() {
     <>
       <Banner />
       <Formulario aoColaboradorCadastrado = {colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Time nome='Programação' />
+      <Time nome='Front-End' />
+      <Time nome='Data Science' />
     </>
   )
 }
